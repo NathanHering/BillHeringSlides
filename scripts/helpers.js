404ed8fa72ed4cb036a5b4e04794d46212120517
Nathan Hering <nathanh@gointerject.com>
2024-09-16 18:52:33 -0400

    update helper

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -5,13 +5,13 @@
  * @param {string} size The size of the image. E.G. 'sm', 'md', '' empty string means full size, for download.
  */
 function GetImagePath(folder, image, size = '') {
-   try {
-      let folderNum = folder.id.toString().padStart(2, '0')
-      let imgNum = image.toString().padStart(3, '0')
-      let suffix = size === '' ? '' : '_' + size
-      return './images/' + folder.path + '/' + folderNum + '_' + imgNum + suffix + '.jpg'
-   } catch (error) {
-      console.log(arguments)
-      console.error(error)
-   }
+   // check to see if the url where this is hosted is file directory or on a github pages
+   let isFile = window.location.protocol === 'file:'
+   
+   let folderNum = folder.id.toString().padStart(2, '0')
+   let imgNum = image.toString().padStart(3, '0')
+   let suffix = size === '' ? '' : '_' + size
+   let root = isFile ? './images/' : './blob/main/images/'
+
+   return root + folder.path + '/' + folderNum + '_' + imgNum + suffix + '.jpg'
 }
